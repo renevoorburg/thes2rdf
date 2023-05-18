1fbc61dd3174c70f0e3ed6669a811178c2efb92b
renevoorburg <voorburg@xs4all.nl>
2023-05-18 08:44:46 +0200

    Update nta2rdf.sh
    better handling of sed variants

--- nta2rdf.sh
+++ nta2rdf.sh
@@ -1,7 +1,8 @@
 #/bin/bash
+set +H
 
 # nta2rdf.sh
-# 2023-05-17
+# 2023-05-18
 
 # Creates rdf from the GGC-THES OAI-PMH dataset that lives at https://services.kb.nl/mdo/oai .
 # Outputs RDF/XML that can be used at http://data.bibliotheken.nl/id/dataset/persons .
@@ -36,12 +37,13 @@ if ! hash uconv 2>/dev/null; then
     echo "Requires uconv. Not found. Exiting."
     exit 1
 fi
+SED="sed"
 if [ "`uname`" == "Darwin" ] ; then
     if ! hash gsed 2>/dev/null; then
         echo "Requires GNU sed. Not found. Exiting."
         exit 1
     fi        
-    alias sed="gsed"
+	SED="gsed"	
 fi
 
 
@@ -62,66 +64,66 @@ fi
 	    natio=$(echo $natio | awk '{print$1}' | egrep "[a-z]*")
 
 	    # continu processing skos
-	    skos=$(echo $line | sed 's@.*\(<skos:Concept.*</skos:Concept>\).*@\1@' - )
+	    skos=$(echo $line | $SED 's@.*\(<skos:Concept.*</skos:Concept>\).*@\1@' - )
 	    line="$skos"
 
 	    #remove until rdf:RDF element
-	    line=$(echo $line | sed 's@^.*<rdf:RDF[^>]*>@@')
+	    line=$(echo $line | $SED 's@^.*<rdf:RDF[^>]*>@@')
 	    # and closing element
-	    line=$(echo $line | sed 's@</rdf:RDF.*$@@')
+	    line=$(echo $line | $SED 's@</rdf:RDF.*$@@')
 
 	    # restrict output to persons
 	    line=$(echo $line | grep "http://data.kb.nl/dataset/Persoonsnamen" -)
 
 	    ## remove stuff we don't want:
-	    line=$(echo $line | sed 's@<dc:type[^>]*>[^<]*</dc:type>@@g')
-	    line=$(echo $line | sed 's@<skos:inScheme[^>]*>@@g')
-	    line=$(echo $line | sed 's@<void:inDataset[^>]*>@@')
-	    #line=$(echo $line | sed 's@<foaf:name[^>]*>[^<]*</foaf:name>@@g')
+	    line=$(echo $line | $SED 's@<dc:type[^>]*>[^<]*</dc:type>@@g')
+	    line=$(echo $line | $SED 's@<skos:inScheme[^>]*>@@g')
+	    line=$(echo $line | $SED 's@<void:inDataset[^>]*>@@')
+	    #line=$(echo $line | $SED 's@<foaf:name[^>]*>[^<]*</foaf:name>@@g')
 
 	    ## rename to our standards:
 	    # retag as schema:Person iso skos:Concept
-	    line=$(echo $line | sed 's@skos:Concept@schema:Person@g')
+	    line=$(echo $line | $SED 's@skos:Concept@schema:Person@g')
 
 	    # add ISNI
 	    if [ ! -z "$isni" ] ; then
-	        line=$(echo $line | sed "s@</schema:Person>@<schema:sameAs rdf:resource=\"http://www.isni.org/isni/$isni\"/></schema:Person>@")
+	        line=$(echo $line | $SED "s@</schema:Person>@<schema:sameAs rdf:resource=\"http://www.isni.org/isni/$isni\"/></schema:Person>@")
 	    fi
 
 	    if [ ! -z "$natio" ] ; then
-	        line=$(echo $line | sed  "s@</schema:Person>@<schema:nationality>$natio</schema:nationality></schema:Person>@")
+	        line=$(echo $line | $SED  "s@</schema:Person>@<schema:nationality>$natio</schema:nationality></schema:Person>@")
 	    fi
 
 	    # schema:name iso skos:prefLabel
-	    line=$(echo $line | sed 's@skos:prefLabel@rdfs:label@g')
+	    line=$(echo $line | $SED 's@skos:prefLabel@rdfs:label@g')
 
 	    # schema:givenName iso foaf:givenName
-	    line=$(echo $line | sed 's@foaf:name@schema:name@g')
-	    line=$(echo $line | sed 's@foaf:givenName@schema:givenName@g')
+	    line=$(echo $line | $SED 's@foaf:name@schema:name@g')
+	    line=$(echo $line | $SED 's@foaf:givenName@schema:givenName@g')
 
 	    #
-	    line=$(echo $line | sed 's@foaf:familyName@schema:familyName@g')
+	    line=$(echo $line | $SED 's@foaf:familyName@schema:familyName@g')
 
 	    # set proper URI (http://data.kb.nl/thesaurus/191687707 =>  http://data.bibliotheken.nl/id/thes/p191687707"
-	    line=$(echo $line | sed 's@data.kb.nl/thesaurus/@data.bibliotheken.nl/id/thes/p@g')
+	    line=$(echo $line | $SED 's@data.kb.nl/thesaurus/@data.bibliotheken.nl/id/thes/p@g')
 
 	    ## rename skos:editorialNote to rdfs:comment
-	    line=$(echo $line | sed 's@skos:editorialNote@schema:description@g')
+	    line=$(echo $line | $SED 's@skos:editorialNote@schema:description@g')
 
 	    # rename skos:scopeNote to rdfs:comment
-	    line=$(echo $line | sed 's@skos:scopeNote@schema:description@g')
+	    line=$(echo $line | $SED 's@skos:scopeNote@schema:description@g')
 
 	    # rename skos:altLabel to schema:alternateName
-	    line=$(echo $line | sed 's@skos:altLabel@schema:alternateName@g')
+	    line=$(echo $line | $SED 's@skos:altLabel@schema:alternateName@g')
 
 	    # rename skos:exactMatch to schema:sameAs
-	    line=$(echo $line | sed 's@skos:exactMatch@schema:sameAs@g')
+	    line=$(echo $line | $SED 's@skos:exactMatch@schema:sameAs@g')
 
 	    # rename skos:related to schema:sameAs
-	    line=$(echo $line | sed 's@skos:related@schema:sameAs@g')
+	    line=$(echo $line | $SED 's@skos:related@schema:sameAs@g')
 
 	    # add 'meta' node:
-	    ppn=$(echo $line | sed 's@.*<schema:Person rdf:about="http://data.bibliotheken.nl/id/thes/p\([0-9Xx]*\)">.*@\1@')
+	    ppn=$(echo $line | $SED 's@.*<schema:Person rdf:about="http://data.bibliotheken.nl/id/thes/p\([0-9Xx]*\)">.*@\1@')
 	    nodedata='<rdf:type rdf:resource="http://schema.org/Dataset"/>'
 	    nodedata="$nodedata"'<owl:sameAs rdf:resource="http://data.bibliotheken.nl/doc/thes/p'$ppn'"/>'
 	    nodedata="$nodedata"'<kbdef:ppn>'$ppn'</kbdef:ppn>'
@@ -132,23 +134,23 @@ fi
 	    nodedata="$nodedata"'<schema:isBasedOn rdf:resource="http://services.kb.nl/mdo/oai?verb=GetRecord\&amp;identifier=GGC-THES:AC:'$ppn'\&amp;metadataPrefix=mdoall"/>'
 	    node="<schema:mainEntityOfPage><schema:WebPage>$nodedata</schema:WebPage></schema:mainEntityOfPage>"
 	    #node=$node'<schema:mainEntityOfPage rdf:resource="http://data.bibliotheken.nl/doc/thes/p'$ppn'"/>'
-	    line=$(echo $line | sed "s@</schema:Person>@$node</schema:Person>@")
+	    line=$(echo $line | $SED "s@</schema:Person>@$node</schema:Person>@")
 
 	    ## fixes:
 	    # remove unknow /empty schema:deathDate
-	    line=$(echo $line | sed 's@<schema:deathDate/>@@')
+	    line=$(echo $line | $SED 's@<schema:deathDate/>@@')
 
 	    # remove unknow /empty schema:birthhDate
-	    line=$(echo $line | sed 's@<schema:birthDate/>@@')
+	    line=$(echo $line | $SED 's@<schema:birthDate/>@@')
 
 	    # add datatype to deathDate:
-	    line=$(echo $line | sed 's@<schema:deathDate>\([0-9]*\)</schema:deathDate>@<schema:deathDate rdf:datatype="http://www.w3.org/2001/XMLSchema#gYear">\1</schema:deathDate>@')
+	    line=$(echo $line | $SED 's@<schema:deathDate>\([0-9]*\)</schema:deathDate>@<schema:deathDate rdf:datatype="http://www.w3.org/2001/XMLSchema#gYear">\1</schema:deathDate>@')
 
 	    # add datatype to birthDate:
-	    line=$(echo $line | sed 's@<schema:birthDate>\([0-9]*\)</schema:birthDate>@<schema:birthDate rdf:datatype="http://www.w3.org/2001/XMLSchema#gYear">\1</schema:birthDate>@')
+	    line=$(echo $line | $SED 's@<schema:birthDate>\([0-9]*\)</schema:birthDate>@<schema:birthDate rdf:datatype="http://www.w3.org/2001/XMLSchema#gYear">\1</schema:birthDate>@')
 
 	    # remove spaces before end tag:
-	    line=$(echo $line | sed 's@\s*</@</@g')
+	    line=$(echo $line | $SED 's@\s*</@</@g')
 
 	    # output
 	    echo $line
